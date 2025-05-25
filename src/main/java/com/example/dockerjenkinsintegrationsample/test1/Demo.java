b47d2453db546f254a9a782a5a215523633f4242
gmnaidu <gmnaidu341@gmail.com>
2025-05-25 20:03:18 +0530

    second commit

---
+++ src/main/java/com/example/dockerjenkinsintegrationsample/test1/Demo.java
@@ -0,0 +1,8 @@
+package com.example.dockerjenkinsintegrationsample.test1;
+
+public class Demo {
+    
+    public static void main(String[] args) {
+        System.out.println("This contain main method...");
+    }
+}
